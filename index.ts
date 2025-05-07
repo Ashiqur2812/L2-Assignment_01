ce6ba6acfb495d6ad459293cd6f4ecbd4db3ffa6
Ashiqur <ashiqur2812@gmail.com>
2025-05-08 01:56:02 +0600

    solved the second problem

--- index.ts
+++ index.ts
@@ -7,3 +7,17 @@ function formatString(input: string, toUpper?: boolean): string {
 }
 console.log(formatString('hello', false));
 console.log(formatString('hello', true));
+
+
+function filterByRating(items: { title: string; rating: number; }[]): { title: string; rating: number; }[] {
+    const res1 = items.filter(e => e.rating >= 4);
+    return res1;
+}
+
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+];
+
+console.log(filterByRating(books));
