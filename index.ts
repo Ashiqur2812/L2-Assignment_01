6e6d107014fb3edaefb501499bd86f9a05065163
Ashiqur <ashiqur2812@gmail.com>
2025-05-08 09:44:30 +0600

    solved the fifth problem

--- index.ts
+++ index.ts
@@ -60,3 +60,14 @@ class Car extends Vehicle {
 const myCar = new Car("Toyota", 2020, "Corolla");
 myCar.getInfo();
 myCar.getModel();
+
+
+function processValue(value: string | number): number {
+    if (typeof value === 'string') {
+        return value.length;
+    } else {
+        return value * 2;
+    }
+}
+console.log(processValue("hello"));
+console.log(processValue(10));
